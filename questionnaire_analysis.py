0529ab052fc48a3acc862bd380d2e2a1c966a7b5
curious <torrpolakow@gmail.com>
2018-07-08 01:10:21 +0300

    full analysis

--- questionnaire_analysis.py
+++ questionnaire_analysis.py
@@ -116,12 +116,13 @@ if __name__ == "__main__":
         # manova_df = creating_dataframe4manova(sf, users_pref_tot)
         # manova_df.to_csv(df_dir + 'manova_df_dataframe.csv')
         sf.pop('rDeployment_tt')
-        for i in sf:
-            stats_df = sf[i]
-            preference_plot(stats_df, 'sub_scale', 'summary', fname='_barplot_only_choices')
-            preference_plot(stats_df, 'sub_scale', 'summary', fname='_summary', deployment=True)
-            # qdf = pair_plot(stats_df, ['BFI','NARS'])
-            # questionnaires_boxplot(qdf, 'feature', 'answers', 'gender')
+        # only analyzing the choices of all the questions asked after each videeo.
+        # for i in sf:
+        #     stats_df = sf[i]
+        #     preference_plot(stats_df, 'sub_scale', 'summary', fname='_barplot_only_choices_'+i[-2:])
+        #     preference_plot(stats_df, 'sub_scale', 'summary', fname='_summary_'+i[-2:], deployment=True)
+        #     qdf = pair_plot(stats_df, ['BFI','NARS'])
+        #     questionnaires_boxplot(qdf, 'feature', 'answers', 'gender')
         preference_cinsistency(users_pref_tot, sf, ignore = False)
         preference_per_question(pref_df_tot) # todo: i think there is something wrong with the order - rationality_irrationality
 
