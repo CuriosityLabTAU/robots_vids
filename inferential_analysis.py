b7a8d876373d577cca90846d54a892d93d16928e
“PTorr” <torrpolakow@gmail.com>
2018-09-02 15:17:27 +0300

    cronbach_alpha

--- inferential_analysis.py
+++ inferential_analysis.py
@@ -636,6 +636,16 @@ def conditional_probability(df, columnA, columnB):
     return conditional_probs
 
 
+def cronbach_alpha(items):
+    items = pd.DataFrame(items)
+    items_count = items.shape[1]
+    variance_sum = float(items.var(axis=0, ddof=1).sum())
+    total_var = float(items.sum(axis=1).var(ddof=1))
+
+    return (items_count / float(items_count - 1) *
+            (1 - variance_sum / total_var))
+
+
 def save_maxfig(fig, fig_name, transperent = False, frmt='png', resize=None):
     '''
     Save figure in high resolution
